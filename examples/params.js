24d354aafa5d45ed8d08075365088f5257884a9e
Cliffano Subagio <cliffano@gmail.com>
2011-02-15 00:33:18 +1100

    Fixed param passing to pages. Updated examples.

--- examples/params.js
+++ examples/params.js
@@ -1,5 +1,17 @@
 exports.params = {
-    title: 'AE86 Garage',
-    pages: [ 'Home', 'Cars', 'Original', 'Contact' ],
-    pageLinks: [ 'home.html', 'cars/list.html', 'cars/original/original.html', 'contact.html' ]
+    name: 'ae86',
+    url: 'http://github.com/cliffano/ae86',
+    title: 'AE86 Printing',
+    nav: {
+        home: { title: 'Home', url: 'index.html'},
+        services: { title: 'Services', url: 'services/index.html'},
+        specialty: { title: 'Specialty', url: 'services/specialty/index.html'},
+        contact: { title: 'Contact Us', url: 'contact.html'}
+    },
+    services: [ 'Business Card', 'Brochure', 'Annual Report' ],
+    specialty: {
+        businesscard: {
+            sizes: [ '55x90mm', '110x99mm' ]
+        }
+    }
 };
