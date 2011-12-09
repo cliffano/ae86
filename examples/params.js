1a661a14eb9fd606a6e084ad4ea85d86b8a6615d
Cliffano Subagio <cliffano@gmail.com>
2011-12-10 01:54:42 +1100

    Add navigation snippet, non-default layout, subdir partial to examples.

--- examples/params.js
+++ examples/params.js
@@ -1,9 +1,10 @@
 exports.params = {
   site: {
-    name: 'Example Inc.',
+    name: 'Examples',
     url: 'http://localhost:3000'
   },
   sitemap: {
-    'index.html': { title: 'Home' }
+    'index.html': { title: 'Home Page' },
+    'about/team.html': { title: 'About - Team Page', layouts: 'alternative.html' }
   }
 };
