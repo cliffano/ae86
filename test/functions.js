71b3f12c91ad1659c4cfac131c94f2976c7d2794
Cliffano Subagio <cliffano@gmail.com>
2012-07-21 00:21:39 +1000

    Check callback done is called.

--- test/functions.js
+++ test/functions.js
@@ -24,18 +24,20 @@ describe('functions', function () {
 
   describe('date', function () {
 
-    it('should return formatted date when format is specified', function () {
+    it('should return formatted date when format is specified', function (done) {
       functions = (create(checks, mocks))('somepage', {}, {});
       functions.date('yyyymmdd', function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('20001010');
     });
 
-    it('should return date with default format when format is not specified', function () {
+    it('should return date with default format when format is not specified', function (done) {
       functions = (create(checks, mocks))('somepage', {}, {});
       functions.date(undefined, function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('2000-10-10T00:00:00');
     });
@@ -43,18 +45,20 @@ describe('functions', function () {
 
   describe('include', function () {
 
-    it('should return partial template when partial exists', function () {
+    it('should return partial template when partial exists', function (done) {
       functions = (create(checks, mocks))('somepage', {}, {});
       functions.include('somepartial', function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('[error] partial somepartial does not exist');
     });
 
-    it('should return error message when partial does not exist', function () {
+    it('should return error message when partial does not exist', function (done) {
       functions = (create(checks, mocks))('somepage', {}, { partials: { somepartial: 'foobar' } });
       functions.include('somepartial', function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('foobar');
     });
@@ -62,34 +66,38 @@ describe('functions', function () {
 
   describe('relative', function () {
 
-    it('should return path when page is at the project root directory', function () {
+    it('should return path when page is at the project root directory', function (done) {
       functions = (create(checks, mocks))('homepage.html', {}, {});
       functions.relative('somepage.html', function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('somepage.html');
     });
 
-    it('should return a single upper directory when page is at a project subdirectory', function () {
+    it('should return a single upper directory when page is at a project subdirectory', function (done) {
       functions = (create(checks, mocks))('foo/homepage.html', {}, {});
       functions.relative('somepage.html', function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('../somepage.html');
     });
 
-    it('should return multiple upper directories when page is at several directories deep below project directory', function () {
+    it('should return multiple upper directories when page is at several directories deep below project directory', function (done) {
       functions = (create(checks, mocks))('foo/bar/homepage.html', {}, {});
       functions.relative('somepage.html', function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('../../somepage.html');
     });
 
-    it('should return multiple upper directories when page and path are both at several directories deep below project directory', function () {
+    it('should return multiple upper directories when page and path are both at several directories deep below project directory', function (done) {
       functions = (create(checks, mocks))('foo/bar/homepage.html', {}, {});
       functions.relative('abc/xyz/somepage.html', function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('../../abc/xyz/somepage.html');
     });
@@ -97,26 +105,29 @@ describe('functions', function () {
 
   describe('title', function () {
 
-    it('should return title when file exists in sitemap and it has a title', function () {
+    it('should return title when file exists in sitemap and it has a title', function (done) {
       functions = (create(checks, mocks))('somepage.html', {}, { sitemap: { 'somepage.html': { title: 'foobar' } } });
       functions.title(function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('foobar');
     });
 
-    it('should return undefined when file exists in sitemap but it does not have a title', function () {
+    it('should return undefined when file exists in sitemap but it does not have a title', function (done) {
       functions = (create(checks, mocks))('somepage.html', {}, { sitemap: { 'somepage.html': {} } });
       functions.title(function (data) {
         checks.data = data;
+        done();
       });
       should.not.exist(checks.data);
     });
 
-    it('should return error message when file does not exist in sitemap', function () {
+    it('should return error message when file does not exist in sitemap', function (done) {
       functions = (create(checks, mocks))('somepage.html', {}, {});
       functions.title(function (data) {
         checks.data = data;
+        done();
       });
       checks.data.should.equal('[error] page somepage.html does not have any sitemap title');
     });
