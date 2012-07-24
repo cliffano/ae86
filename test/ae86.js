6fbe8efd00a8bef5ce6176f77dec5d35621f6ba3
Cliffano Subagio <cliffano@gmail.com>
2012-07-24 22:38:19 +1000

    Add AE86#watch tests.

--- test/ae86.js
+++ test/ae86.js
@@ -1,190 +1,220 @@
-var _ = require('underscore'),
-  assert = require('assert'),
-  jscoverageHack = require('../lib/ae86'),
+var bag = require('bagofholding'),
+  _jscov = require('../lib/ae86'),
   sandbox = require('sandboxed-module'),
-  vows = require('vows');
+  should = require('should'),
+  checks, mocks,
+  ae86;
 
-vows.describe('ae86').addBatch({
-  'init': {
-    topic: function () {
-      return function (checks) {
-        return sandbox.require('../lib/ae86', {
-          requires: {
-            ncp: {
-              ncp: function (src, dest, cb) {
-                assert.equal(dest, '.');
-                checks.src = src;
-                cb();
-              }
-            }
-          }
-        });
-      };
-    },
-    'should call ncp recursive copy with specified source dir': function (topic) {
-      var _err,
-        checks = {},
-        ae86 = new topic(checks).AE86();
-      ae86.init('examples', function (err) {
-        _err = err;
-      });
-      assert.equal(checks.src, 'examples');
-      assert.isUndefined(_err);
-    }
-  },
-  'gen': {
-    topic: function () {
-      return function (checks) {
-        return sandbox.require('../lib/ae86', {
-          requires: {
-            dateformat: function (format) {
-              assert.equal(format, 'yyyymmddHHMMssLl');
-              return 'mydate';
-            },
-            ncp: {
-              ncp: function (src, dest, cb) {
-                checks.statik = {
-                  src: src,
-                  dest: dest
-                };
-                cb();
-              }
-            },
-            './engine': {
-              compile: function (dir, cb) {
-                checks.compileDirs.push(dir);
-                cb('compiled ' + dir);
-              },
-              process: function (dir, pages, layouts, partials, params, cb) {
-                checks.processTemplates = {
-                  dir: dir,
-                  pages: pages,
-                  layouts: layouts,
-                  partials: partials,
-                  params: params
-                };
-                cb(null, ['index.html', 'contact.html']);
-              }
-            }
-          }
-        });
-      };
-    },
-    'should use default directories when no options specified': function (topic) {
-      var _results,
-        checks = { compileDirs: [] },
-        ae86 = new topic(checks).AE86();
-      ae86.gen({ foo: 'bar' }, function (err, results) {
-        assert.isUndefined(err);
-        _results = results;
-      });
-      assert.equal(checks.statik.src, 'static');
-      assert.equal(checks.statik.dest, 'out');
-      assert.equal(checks.compileDirs.length, 3);
-      assert.equal(checks.compileDirs[0], 'partials');
-      assert.equal(checks.compileDirs[1], 'layouts');
-      assert.equal(checks.compileDirs[2], 'pages');
-      assert.equal(checks.processTemplates.dir, 'out');
-      assert.equal(checks.processTemplates.pages, 'compiled pages');
-      assert.equal(checks.processTemplates.layouts, 'compiled layouts');
-      assert.equal(checks.processTemplates.partials, 'compiled partials');
-      assert.equal(checks.processTemplates.params.foo, 'bar');
-      assert.isEmpty(checks.processTemplates.params.sitemap);
-      assert.equal(checks.processTemplates.params.__genId, 'mydate');
-    },
-    'should use custom directories when options specified': function (topic) {
-      var _results,
-        checks = { compileDirs: [] },
-        ae86 = new topic(checks).AE86({
-          out: 'myout',
-          layouts: 'mylayouts',
-          pages: 'mypages',
-          partials: 'mypartials',
-          statik: 'mystatic'
-        });
-      ae86.gen({ foo: 'bar' }, function (err, results) {
-        assert.isUndefined(err);
-        _results = results;
-      });
-      assert.equal(checks.statik.src, 'mystatic');
-      assert.equal(checks.statik.dest, 'myout');
-      assert.equal(checks.compileDirs.length, 3);
-      assert.equal(checks.compileDirs[0], 'mypartials');
-      assert.equal(checks.compileDirs[1], 'mylayouts');
-      assert.equal(checks.compileDirs[2], 'mypages');
-      assert.equal(checks.processTemplates.dir, 'myout');
-      assert.equal(checks.processTemplates.pages, 'compiled mypages');
-      assert.equal(checks.processTemplates.layouts, 'compiled mylayouts');
-      assert.equal(checks.processTemplates.partials, 'compiled mypartials');
-      assert.equal(checks.processTemplates.params.foo, 'bar');
-      assert.isEmpty(checks.processTemplates.params.sitemap);
-      assert.equal(checks.processTemplates.params.__genId, 'mydate');
-    }
-  },
-  'watch': {
-    topic: function () {
-      return function (checks) {
-        return sandbox.require('../lib/ae86', {
-          requires: {
-            'watch-tree-maintained': {
-              watchTree: function (file, options) {
-                assert.isTrue(options.ignore !== undefined);
-                assert.equal(options['sample-rate'], 5);
-                checks.files.push(file);
-                return {
-                  on: function (event, cb) {
-                    checks.events.push(event);
-                    cb();
-                  }
-                };
-              }
-            }
-          }
-        });
-      };
-    },
-    'should watch default directories and params file with specified listener when no options specified': function (topic) {
-      var checks = { files: [], events: [] },
-        listenerCallCount = 0,
-        listener = function() {
-          listenerCallCount += 1;
-        },
-        ae86 = new topic(checks).AE86();        
-      ae86.watch(listener);
-      assert.equal(checks.files.length, 5);
-      assert.equal(checks.files[0], 'layouts');
-      assert.equal(checks.files[1], 'pages');
-      assert.equal(checks.files[2], 'params.js');
-      assert.equal(checks.files[3], 'partials');
-      assert.equal(checks.files[4], 'static');
-      // each file/dir fires 3 events (5 * 3 = 15)
-      assert.equal(checks.events.length, 15);
-      assert.equal(listenerCallCount, 15);
-    },
-    'should watch custom directories and params file with specified listener when options specified': function (topic) {
-      var checks = { files: [], events: [] },
-        listenerCallCount = 0,
-        listener = function() {
-          listenerCallCount += 1;
-        },
-        ae86 = new topic(checks).AE86({
-          out: 'myout',
-          layouts: 'mylayouts',
-          pages: 'mypages',
-          params: 'myparams',
-          partials: 'mypartials',
-          statik: 'mystatic'
-        });        
-      ae86.watch(listener);
-      assert.equal(checks.files.length, 5);
-      assert.equal(checks.files[0], 'mylayouts');
-      assert.equal(checks.files[1], 'mypages');
-      assert.equal(checks.files[2], 'myparams.js');
-      assert.equal(checks.files[3], 'mypartials');
-      assert.equal(checks.files[4], 'mystatic');
-      // each file/dir fires 3 events (5 * 3 = 15)
-      assert.equal(checks.events.length, 15);
-      assert.equal(listenerCallCount, 15);
-    }
+describe('ae86', function () {
+
+  function create(checks, mocks) {
+    return sandbox.require('../lib/ae86', {
+      requires: mocks ? mocks.requires : {},
+      globals: {
+        console: bag.mock.console(checks, mocks),
+        process: bag.mock.process(checks, mocks)
+      },
+      locals: {
+        __dirname: '/somepath/ae86/lib'
+      }
+    });
   }
-}).exportTo(module);
+
+  beforeEach(function () {
+    checks = {};
+    mocks = {};
+  });
+
+  describe('init', function () {
+
+    it('should delegate to ncp ncp when initialising the project', function (done) {
+      mocks.requires = {
+        ncp: {
+          ncp: function (srcDir, destDir, cb) {
+            checks.ncp_ncp_srcDir = srcDir;
+            checks.ncp_ncp_destDir = destDir;
+            cb();
+          }
+        }
+      };
+      ae86 = new (create(checks, mocks))();
+      ae86.init(function (err, result) {
+        done();
+      });
+      checks.ncp_ncp_srcDir.should.equal('/somepath/ae86/examples');
+      checks.ncp_ncp_destDir.should.equal('.');
+      checks.console_log_messages.length.should.equal(1);
+      checks.console_log_messages[0].should.equal('Creating example AE86 project');
+    });
+  });
+
+  describe('generate', function () {
+
+    beforeEach(function () {
+
+    });
+
+    it('should set default params', function () {
+    });
+
+    it('should copy static files', function () {
+    });
+
+    it('should compile page templates', function () {
+    });
+  });
+
+  describe('watch', function () {
+
+    beforeEach(function () {
+
+      checks.ae86_generate__count = 0;
+      checks.ae86_clean__count = 0;
+      checks.watchtree_watchTree_args = {};
+
+      mocks.process_cwd = '/somepath';
+      mocks.requires = {
+        'watch-tree-maintained': {
+          watchTree: function (file, opts) {
+            checks.watchtree_watchTree_args[file] = opts;
+            return bag.mock.stream(checks, mocks);
+          }
+        }
+      };
+
+      require.cache['/somepath/params.js'] = { foo: 'bar' };
+
+      ae86 = new (create(checks, mocks))();
+    });
+
+    it('should log activity message', function () {
+      ae86.watch();
+
+      checks.console_log_messages.length.should.equal(1);
+      checks.console_log_messages[0].should.equal('Watching for changes and automatically regenerating website');
+    });
+
+    it('should ignore swap files and set sample rate on project directories and files', function () {
+      ae86.watch();
+
+      checks.watchtree_watchTree_args['static'].ignore.should.equal('\\.swp');
+      checks.watchtree_watchTree_args['static']['sample-rate'].should.equal(5);
+
+      checks.watchtree_watchTree_args['partials'].ignore.should.equal('\\.swp');
+      checks.watchtree_watchTree_args['partials']['sample-rate'].should.equal(5);
+
+      checks.watchtree_watchTree_args['layouts'].ignore.should.equal('\\.swp');
+      checks.watchtree_watchTree_args['layouts']['sample-rate'].should.equal(5);
+
+      checks.watchtree_watchTree_args['pages'].ignore.should.equal('\\.swp');
+      checks.watchtree_watchTree_args['pages']['sample-rate'].should.equal(5);
+
+      checks.watchtree_watchTree_args['params.js'].ignore.should.equal('\\.swp');
+      checks.watchtree_watchTree_args['params.js']['sample-rate'].should.equal(5);
+    });
+
+    it('should log message and set listener when a file is created', function () {
+
+      mocks.stream_on_fileCreated = ['somepath', 'somestats'];
+
+      ae86 = new (create(checks, mocks))();
+      ae86.generate = function () {
+        checks.ae86_generate__count++;
+      };
+
+      should.exist(require.cache['/somepath/params.js']);
+
+      ae86.watch();
+
+      should.not.exist(require.cache['/somepath/params.js']);
+
+      checks.ae86_generate__count.should.equal(5);
+
+      checks.console_log_messages.length.should.equal(6);
+      checks.console_log_messages[0].should.equal('Watching for changes and automatically regenerating website');
+      checks.console_log_messages[1].should.equal('static was created');
+      checks.console_log_messages[2].should.equal('partials was created');
+      checks.console_log_messages[3].should.equal('layouts was created');
+      checks.console_log_messages[4].should.equal('pages was created');
+      checks.console_log_messages[5].should.equal('params.js was created');      
+    });
+
+    it('should log message and set listener when a file is modified', function () {
+
+      mocks.stream_on_fileModified = ['somepath', 'somestats'];
+
+      ae86 = new (create(checks, mocks))();
+      ae86.generate = function () {
+        checks.ae86_generate__count++;
+      };
+
+      should.exist(require.cache['/somepath/params.js']);
+
+      ae86.watch();
+
+      should.not.exist(require.cache['/somepath/params.js']);
+
+      checks.ae86_generate__count.should.equal(5);
+
+      checks.console_log_messages.length.should.equal(6);
+      checks.console_log_messages[0].should.equal('Watching for changes and automatically regenerating website');
+      checks.console_log_messages[1].should.equal('static was modified');
+      checks.console_log_messages[2].should.equal('partials was modified');
+      checks.console_log_messages[3].should.equal('layouts was modified');
+      checks.console_log_messages[4].should.equal('pages was modified');
+      checks.console_log_messages[5].should.equal('params.js was modified');      
+    });
+
+    it('should log message, call clean, and set listener when a file is deleted', function () {
+    
+      mocks.stream_on_fileDeleted = ['somepath', 'somestats'];
+
+      ae86 = new (create(checks, mocks))();
+      ae86.generate = function () {
+        checks.ae86_generate__count++;
+      };
+      ae86.clean = function () {
+        checks.ae86_clean__count++;
+      };
+
+      should.exist(require.cache['/somepath/params.js']);
+      
+      ae86.watch();
+
+      should.not.exist(require.cache['/somepath/params.js']);
+
+      checks.ae86_generate__count.should.equal(5);
+      checks.ae86_clean__count.should.equal(5);
+
+      checks.console_log_messages.length.should.equal(6);
+      checks.console_log_messages[0].should.equal('Watching for changes and automatically regenerating website');
+      checks.console_log_messages[1].should.equal('static was deleted');
+      checks.console_log_messages[2].should.equal('partials was deleted');
+      checks.console_log_messages[3].should.equal('layouts was deleted');
+      checks.console_log_messages[4].should.equal('pages was deleted');
+      checks.console_log_messages[5].should.equal('params.js was deleted');  
+    });
+
+  });
+
+  describe('clean', function () {
+
+    it('should delegate to wrench rmdirRecursive when removing the generated website', function (done) {
+      mocks.requires = {
+        wrench: {
+          rmdirRecursive: function (dir, cb) {
+            checks.wrench_rmdirRecursive_dir = dir;
+            cb();
+          }
+        }
+      };
+      ae86 = new (create(checks, mocks))();
+      ae86.clean(function (err, result) {
+        done();
+      });
+      checks.wrench_rmdirRecursive_dir.should.equal('out');
+      checks.console_log_messages.length.should.equal(1);
+      checks.console_log_messages[0].should.equal('Removing website');
+    });
+  });
+});
