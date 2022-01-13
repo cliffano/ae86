4655faf8098786cbd7dd13e392afe64814a1826f
cliffano <cliffano@users.noreply.github.com>
2022-01-13 10:26:53 +0000

    deploy: a1fb6a02862e18ec15ab2a5daa5ba1aa510b3714

--- complexity/plato/files/lib_functions_js/report.history.js
+++ complexity/plato/files/lib_functions_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 13 Jan 2022 10:26:17 GMT","sloc":76,"lloc":21,"functions":5,"deliveredBugs":0.26,"difficulty":26,"maintainability":74.491,"lintErrors":2}]
+__history = [{"date":"Thu, 13 Jan 2022 10:26:43 GMT","sloc":76,"lloc":21,"functions":5,"deliveredBugs":0.26,"difficulty":26,"maintainability":74.491,"lintErrors":2}]
--- complexity/plato/report.history.js
+++ complexity/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 13 Jan 2022 10:26:17 GMT","total":{"sloc":76,"maintainability":74.491},"average":{"sloc":76,"maintainability":"74.49"}}]
+__history = [{"date":"Thu, 13 Jan 2022 10:26:44 GMT","total":{"sloc":76,"maintainability":74.491},"average":{"sloc":76,"maintainability":"74.49"}}]
